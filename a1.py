c865c9b28aab09f0bc7d6b8ed1a3eae6295e59c9
aishwaryapoor <118243138+aishwaryapoor@users.noreply.github.com>
2023-01-20 12:34:37 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third numbert "))
+if a>b and a>c:
+    print("a is greater")
+if b>a and b>c:
+    print("b is greater")
+if c>a and c>b:
+    print("c is greater")
+    
+
+        
